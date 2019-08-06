613dca04de71a09e154c3433ab76c69371e21419
luisaguterres <@Luisa27>
2019-08-06 18:21:45 -0300

    Show the list books from library

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+       System.out.println(welcomeMessage());
+       Library library = new Library();
+        System.out.printf(library.getBooks().toString());
     }
+
+    public static String welcomeMessage(){return "Welcome to Biblioteca. Your one-step-shop for great book titles in Bangalore!";}
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,15 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+import static org.hamcrest.core.IsInstanceOf.any;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldReturnWelcomeMessage() {
+        assertTrue(BibliotecaApp.welcomeMessage() instanceof String);
     }
 }
