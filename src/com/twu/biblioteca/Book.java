0b5fe429f29e858dca22f5d084260b6e0377c566
luisaguterres <@Luisa27>
2019-08-12 10:06:58 -0300

    The class book and the tests of class library.

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,41 @@
+package com.twu.biblioteca;
+
+import java.util.UUID;
+
+public class Book {
+
+    private String id;
+    private String title;
+    private int publishYear;
+    private String author;
+    private boolean checkout;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "title '" + title + '\'' +
+                ", publishYear " + publishYear +
+                ", author " + author + '\'' +
+                ", checkout " + checkout + '\'' +
+                '}';
+    }
+
+    public Book(String title, int publishYear, String author) {
+        UUID uuid = UUID.randomUUID();
+        this.id = uuid.toString();
+        this.title = title;
+        this.publishYear = publishYear;
+        this.author = author;
+    }
+
+    public Object getTitle() {
+        return this.title;
+    }
+
+    public boolean hasBeenCheckouted(){
+        return this.checkout;
+    }
+
+    public void changeBookStatus(boolean status) { this.checkout = status; }
+
+}
---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class LibraryTest {
+
+    @Test
+    public void shouldReturnLibraryBooksList() {
+        Library library = new Library();
+       // assertTrue(library.getBooks() instanceof ArrayList);
+    }
+}
